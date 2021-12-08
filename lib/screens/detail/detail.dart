a3c5a7ac215b54e741d1bea7ce7880f830135df7
Humza Khan <83923770+HumzaKhan071@users.noreply.github.com>
2021-12-08 19:50:59 +0500

    update

--- lib/screens/detail/detail.dart
+++ lib/screens/detail/detail.dart
@@ -1,12 +1,14 @@
 import 'package:e_learning_app/models/course.dart';
+import 'package:e_learning_app/models/module.dart';
 import 'package:e_learning_app/screens/detail/widgets/course_desc.dart';
 import 'package:e_learning_app/screens/detail/widgets/course_progress.dart';
 import 'package:e_learning_app/screens/detail/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
+ 
   final Course course;
-  const DetailPage({Key? key, required this.course}) : super(key: key);
+DetailPage({Key? key, required this.course}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +20,9 @@ class DetailPage extends StatelessWidget {
             CustomAppBar(
               course: course,
             ),
-            CourseDescription( course: course,),
+            CourseDescription(
+              course: course,
+            ),
             CourseProgress(),
           ],
         ),
--- lib/screens/detail/widgets/course_progress.dart
+++ lib/screens/detail/widgets/course_progress.dart
@@ -1,12 +1,50 @@
+import 'package:e_learning_app/constants/colors.dart';
+import 'package:e_learning_app/models/module.dart';
 import 'package:flutter/material.dart';
 
+import 'course_module.dart';
+
 class CourseProgress extends StatelessWidget {
-  const CourseProgress({ Key? key }) : super(key: key);
+   final moduleList = Module.generateModules();
+CourseProgress({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: EdgeInsets.all(25),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "The Progress",
+                style: TextStyle(
+                    fontWeight: FontWeight.bold, fontSize: 20, color: kFont),
+              ),
+              Row(
+                children: [
+                  Image.asset(
+                    "assets/icons/grid.png",
+                    width: 25,
+                  ),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Image.asset(
+                    "assets/icons/list.png",
+                    width: 25,
+                  )
+                ],
+              )
+            ],
+          ),
+          SizedBox(height: 20,),
+
+          ...moduleList.map((e) => CourseModule(module: e)).toList()
+         
+        ],
+      ),
     );
   }
 }
